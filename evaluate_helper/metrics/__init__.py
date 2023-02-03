3408e29f1334d975db5ff1a92552f41776747d77
automatethem <108699325+automatethem@users.noreply.github.com>
2023-02-03 10:35:10 +0900

    Update __init__.py

--- evaluate_helper/metrics/__init__.py
+++ evaluate_helper/metrics/__init__.py
@@ -1,4 +1,9 @@
-from .custom_accuracy import *
-from .mean_average_precision import *
-from .psnr import *
-from .average_metrics import *
+from . import custom_accuracy
+from . import mean_average_precision
+from . import psnr
+from . import average_metrics
+
+from .custom_accuracy import CustomAccuracy
+from .mean_average_precision import MeanAveragePrecision
+from .psnr import Psnr
+from .average_metrics import AverageMetrics
