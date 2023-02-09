6cfb1f9536f0b3b8019e7927565b0cf0315c533f
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-02-09 23:33:50 +0900

    Update mean_average_precision.py

--- evaluate_helper/metrics/mean_average_precision.py
+++ evaluate_helper/metrics/mean_average_precision.py
@@ -3,30 +3,6 @@ import evaluate
 import datasets
 from torchmetrics.detection.mean_ap import MeanAveragePrecision as MAP
 
-'''
-#https://medium.com/data-science-at-microsoft/how-to-smoothly-integrate-meanaverageprecision-into-your-training-loop-using-torchmetrics-7d6f2ce0a2b3
-import evaluate
-import evaluate_helper
-train_metrics = evaluate.combine([
-    evaluate_helper.metrics.MeanAveragePrecision()
-])
-predictions = [
-    {
-        'boxes': torch.Tensor([[258.0, 41.0, 606.0, 285.0]]), 
-        'scores': torch.Tensor([0.536]),
-        'labels': torch.Tensor([0])
-    }]
-references=[
-    {
-        'boxes': torch.Tensor([[214.0, 41.0, 562.0, 285.0]]), 
-        'labels': torch.Tensor([0])
-    }
-]
-#train_metrics.add_batch(predictions=predictions, references=references)
-#train_metrics_value = train_metrics.compute()
-train_metrics_value = train_metrics.compute(predictions=[{predictions=predictions, references=references)
-print(train_metrics_value['mean_average_precision']) #0.6000000238418579
-'''
 #evaluate 패키지에서 아직 지원되지 않는 mAP 를 사용하기 위해 evaluate 패키지의 사용자 정의 매트릭 클래스를 구현
 #MeanAveragePrecision: 클래스별로 Average Precision (Precision-Recall 곡선의 아래쪽 면적) 를 구한후 그 값을 평균.
 #mAP 값이 클수록 좋음.
